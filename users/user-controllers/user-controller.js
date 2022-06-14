c1c3758a2178d11915fbea0149f2a02c36b02380
CalmDeveloper <demir445@gmail.com>
2022-06-14 14:24:25 +0300

    hw3

--- users/user-controllers/user-controller.js
+++ users/user-controllers/user-controller.js
@@ -40,9 +40,9 @@ const createUser = async (req, res) => {
             return res.status(400).json('enter valid age')
         }
         const users = await fileService.reader()
-        const lastId = users[users.length - 1]
-        const newUser = {id: lastId.id + 1, name, age}
-        const newUsers = [...users, {id: lastId.id + 1, name, age}]
+        const lastUser = users[users.length - 1]
+        const newUser = {id: lastUser.id + 1, name, age}
+        const newUsers = [...users, {id: lastUser.id + 1, name, age}]
         await fileService.writer(newUsers)
         return res.json(`user: id: ${newUser.id}, name: ${newUser.name}, age:${newUser.age}  created!`)
     } catch (e) {
@@ -58,9 +58,9 @@ const deleteUser =  async (req, res) => {
             if (findIndex === -1) {
                 return res.status(404).json(`user with userId ${+userId} no found`)
             } else {
-                res.json('user was deleted!')
                 users.splice(findIndex, 1)
-                return await fileService.writer(users)
+                 await fileService.writer(users)
+                return res.json('user was deleted!')
             }
         } else {
             return res.status(400).json(`enter valid userId`)
@@ -74,6 +74,13 @@ const updateUser = async (req, res) => {
         const {userId} = req.params;
         const {name, age} = req.body
         const users = await fileService.reader()
+
+        if (typeof name !== "string" && name.length < 3) {
+            return res.status(400).json('enter valid username')
+        }
+        if (typeof +age !== 'number' && +age % 1 !== 0 && age < 0 || age > 130) {
+            return res.status(400).json('enter valid age')
+        }
         if (typeof +userId !== 'number' && +userId % 1 !== 0) {
             return res.status(400).json(`enter valid userId`)
         }
@@ -82,21 +89,15 @@ const updateUser = async (req, res) => {
             return res.status(404).json(`user with userId ${+userId} no found`)
         }
 
-        if (typeof name !== "string" && name.length < 3) {
-            return res.status(400).json('enter valid username')
-        }
-        if (typeof +age !== 'number' && +age % 1 !== 0 && age < 0 || age > 130) {
-            return res.status(400).json('enter valid age')
-        }
+
         const index = users.findIndex(user => user.id === +userId)
         const updatedUser = Object.assign(users[index], req.body)
         users.splice(index,1)
         await fileService.writer([...users, updatedUser])
-        return res.json('user updated')
+        return res.json(`user: id: ${updatedUser.id}, name: ${updatedUser.name}, age:${updatedUser.age}  updated!`)
     }catch (e) {
         res.status(400).json(e.message || 'Unknown Error');
     }
-
 }
 
 
